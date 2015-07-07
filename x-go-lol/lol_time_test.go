6b9c77c74ea9cb3d615b2f0a64efba3c31601238
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2015-07-07 11:48:46 +0200

    Improves time conversion testing

--- x-go-lol/lol_time_test.go
+++ x-go-lol/lol_time_test.go
@@ -13,7 +13,7 @@ var _ = Suite(&LolTimeSuite{})
 
 func Test(t *testing.T) { TestingT(t) }
 
-func (s *LolTimeSuite) TestCanBeParsed(c *C) {
+func (s *LolTimeSuite) TestCanBeParsedAndFormated(c *C) {
 	validData := map[string]time.Time{
 		"Jul 2, 2015 10:47:51 AM": time.Date(2015, 7, 2, 10, 47, 51, 0, time.UTC),
 		"Jul 2, 2015 10:48:21 PM": time.Date(2015, 7, 2, 22, 48, 21, 0, time.UTC),
@@ -21,13 +21,40 @@ func (s *LolTimeSuite) TestCanBeParsed(c *C) {
 
 	for text, value := range validData {
 		var t LolTime
+		continueTests := false
 		if c.Check(t.UnmarshalText([]byte(text)), IsNil) == true {
 			c.Check(t.Time, Equals, value)
+			continueTests = true
 		}
 
 		if c.Check(t.UnmarshalJSON([]byte(`"`+text+`"`)), IsNil) == true {
 			c.Check(t.Time, Equals, value)
+			continueTests = true
+		}
+
+		if continueTests == false {
+			continue
+		}
+
+		marshaled, err := t.MarshalText()
+		if c.Check(err, IsNil) == true {
+			c.Check(string(marshaled), Equals, text)
+		}
+		marshaled, err = t.MarshalJSON()
+		if c.Check(err, IsNil) == true {
+			c.Check(string(marshaled), Equals, `"`+text+`"`)
 		}
 
 	}
+}
+
+func (s *LolTimeSuite) TestDurationCanConvertWithNative(c *C) {
+	data := []DurationMs{
+		10, 42, 100000,
+	}
+
+	for _, dInMs := range data {
+		c.Check(dInMs.Duration(), Equals, time.Duration(dInMs)*time.Millisecond)
+	}
+
 }
