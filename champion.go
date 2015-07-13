04a1c8e4bb2165852f7aefc523bc6eee0da9f9cf
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2015-07-13 18:27:58 +0200

    Adds a way to get Champion Data from the static data api

--- champion.go
+++ champion.go
@@ -1,4 +1,51 @@
 package lol
 
+import "fmt"
+
 // ChampionID is a unique identifier for a Champion
 type ChampionID int //for sure there will never even be more than a thousand champions/
+
+// A Champion can be controlled by a Summoner in a Game
+type Champion struct {
+	AllyTips  []string `json:"allytips"`
+	Blurb     string   `json:"blurb"`
+	EnemyTips []string `json:"enemytips"`
+	ID        int      `json:"id"`
+	Image     ImageDto `json:"image"`
+	Info      struct {
+		Attack     int
+		Defense    int
+		Difficulty int
+		Magic      int
+	} `json:"info"`
+	Key     string `json:"key"`
+	Lore    string `json:"lore"`
+	Name    string `json:"name"`
+	Partype string `json:"partype"`
+	Passive struct {
+		Description          string   `json:"description"`
+		Image                ImageDto `json:"image"`
+		Name                 string   `json:"name"`
+		SanitizedDescription string   `json:"sanitizedDescription"`
+	} `json:"passive"`
+	Recommended []struct{} `json:"recommended"`
+	Skins       []struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+		Num  int    `json:"num"`
+	} `json:"skins"`
+	Spells []struct{} `json:"spells"`
+	Stats  struct{}   `json:"stats"`
+	Tags   []string   `json:"tags"`
+	Title  string     `json:"title"`
+}
+
+// GetChampion returns the champion data for the current patch
+func (a *StaticAPIEndpoint) GetChampion(id ChampionID) (*Champion, error) {
+	res := &Champion{}
+	err := a.cachedGet(fmt.Sprintf("/champion/%d", id), map[string]string{"champData": "all"}, res)
+	if err != nil {
+		return nil, err
+	}
+	return res, err
+}
